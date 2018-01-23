b7aeee2935d9fecd988d04b97fde728c8d607994
Charles-ccc <liu930505@qq.com>
2018-01-23 10:35:29 +0800

    spider

--- index.js
+++ index.js
@@ -98,69 +98,85 @@ app.get('/testCheerio', function(res,res){
     res.send($.html());
 })
 
-app.get('/play_list/:playlistId', function (req, res) {
+app.get('/playlist/:playlistId', function (req, res) {
     // 获得歌单ID
-    var playlistId = res.params.playlistId;
-    // 定义请求 url
-    var requestUrl = 'http://music.163.com/playlist?id=' + playlistId;
+    var playlistId = req.params.playlistId;
     var resObj = {
         code: 200,
         message: "加载成功",
         data: {}
     }
 
-    if (checkId(playlistId)) {
-        request.get(requestUrl)
-            .end(function (err, _response) {
-                if (!err) {
-                    var playlist = {
-                        id: playlistId
-                    };
-                    // 成功返回 HTML, decodeEntities 指定不把中文字符转为 unicode 字符
-                    // 如果不指定 decodeEntities 为 false , 例如 " 会解析为 &quot;
-                    var $ = cherrio.load(_response.text, { decodeEntities: false });
-                    // 获得歌单 dom
-                    var dom = $('#m-playlist');
-                    // 歌单标题
-                    playlist.title = dom.find('.tit').text();
-                    // 歌单拥有者
-                    playlist.owner = dom.find('.user').find('.name').text();
-                    // 创建时间
-                    playlist.create_time = dom.find('.user').find('.time').text();
-                    // 歌单被收藏数量
-                    playlist.collection_count = dom.find('#content-operation').find('.u-btni-fav').attr('data-count');
-                    // 分享数量
-                    playlist.share_count = dom.find('#content-operation').find('.u-btni-share').attr('data-count');
-                    // 评论数量
-                    playlist.comment_count = dom.find('#content-operation').find('#cnt_comment_count').html();
-                    // 标签
-                    playlist.tags = [];
-                    dom.find('.tags').eq(0).find('.u-tag').each(function (index, element) {
-                        playlist.tags.push($(element).text());
-                    });
-                    // 歌单描述
-                    playlist.desc = dom.find('#album-desc-more').html();
-                    // 歌曲总数量
-                    playlist.song_count = dom.find('#playlist-track-count').text();
-                    // 播放总数量
-                    playlist.play_count = dom.find('#play-count').text();
+    request.get(`http://music.163.com/playlist?id=${playlistId}`)
+        .end(function (err, _response) {
+            if (!err) {
+                var playlist = {
+                    id: playlistId
+                };
+                // 成功返回 HTML, decodeEntities 指定不把中文字符转为 unicode 字符
+                // 如果不指定 decodeEntities 为 false , 例如 " 会解析为 &quot;
+                var $ = cheerio.load(_response.text, { decodeEntities: false });
+                // 获得歌单 dom
+                var dom = $('#m-playlist');
+                // 歌单标题
+                playlist.title = dom.find('.tit').text();
+                // 歌单拥有者
+                playlist.owner = dom.find('.user').find('.name').text();
+                // 创建时间
+                playlist.create_time = dom.find('.user').find('.time').text();
+                // 歌单被收藏数量
+                playlist.collection_count = dom.find('#content-operation').find('.u-btni-fav').attr('data-count');
+                // 分享数量
+                playlist.share_count = dom.find('#content-operation').find('.u-btni-share').attr('data-count');
+                // 评论数量
+                playlist.comment_count = dom.find('#content-operation').find('#cnt_comment_count').html();
+                // 标签
+                playlist.tags = [];
+                dom.find('.tags').eq(0).find('.u-tag').each(function (index, element) {
+                    playlist.tags.push($(element).text());
+                });
+                // 歌单描述
+                playlist.desc = dom.find('#album-desc-more').html();
+                // 歌曲总数量
+                playlist.song_count = dom.find('#playlist-track-count').text();
+                // 播放总数量
+                playlist.play_count = dom.find('#play-count').text();
 
-                    resObj.data = playlist;
-                } else {
-                    resObj.code = 404;
-                    resObj.message = "获取API出现问题";
-                    console.log('Get data error!');
-                }
-                res.send(resObj)
-            })
-    } else {
-        resObj.code = 404;
-        resObj.message = "参数异常";
-        res.send(resObj)
-    }
+                resObj.data = playlist;
+            } else {
+                resObj.code = 404;
+                resObj.message = "获取API出现问题";
+                console.log('Get data error!');
+            }
+            res.send(resObj)
+        })
 })
 
+// 定义根据歌单id获得歌单所有歌曲列表的API
+app.get('/song_list/:playlistId', function (req, res) {
 
+    // 获得歌单ID
+    var playlistId = req.params.playlistId;
+    // 定义返回对象
+    var resObj = {
+        code: 200,
+        data: []
+    };
+    request.get(`http://music.163.com/playlist?id=${playlistId}`)
+        .end(function (err, _response) {
+            if (!err) {
+                // 成功返回 HTML
+                var $ = cheerio.load(_response.text, { decodeEntities: false });
+                // 获得歌单 dom
+                var dom = $('#m-playlist');
+                resObj.data = JSON.parse(dom.find('#song-list-pre-cache').find('textarea').html());
+            } else {
+                resObj.code = 404;
+                console.log('Get data error!');
+            }
+            res.send(resObj);
+        });
+});
 
 /**
  * 开启express服务,监听本机3000端口
